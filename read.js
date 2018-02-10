5155b61fb4b2664ab36023e8ee8e58095368a0ab
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-02-10 10:11:05 -0500

    Update read.js

--- read.js
+++ read.js
@@ -1,1 +1,14 @@
-
+var codes = {
+javascript: {
+a: ['say'],
+  b: [function(params) {alert(params[1]);}]
+}
+};
+var parameters = [];
+var js = ['say'];
+function readFunction(code) {
+parameters = code.split("/");
+  if (js.indexOf(parameters[0])!==-1) {
+codes.javascript.b[codes.javascript.a.indexOf(parameters[0])](parameters);
+}
+}
