3eef4a9769106fb7b18ffb61363c1d467ae1ce0a
kyleplo <31634240+kyleplo@users.noreply.github.com>
2018-02-10 16:47:36 -0500

    Sorry I keep messing up

--- core.js
+++
@@ -1,6 +0,0 @@
-function UltraLang(s){
-if(s.indexOf(":") < 5){if(s.toLowerCase.startsWith("gpio:")){this.type = "gpioPin";this.content = new UltraPin(s.slice(5))}else{this.type = "externalRequest";this.content = new UltraRequest(s,true)}};
-if(s.startsWith("/")){this.type = "internalRequest";this.content = new UltraRequest(s,false)};
-if(s.startsWith(".") || s.startsWith("#") || s.startsWith("<") || !this.type){this.type = "element"};
-}
-function u(selector){return new UltraLang(selector)}
