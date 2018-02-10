01d220a92bdce42de59747b98bd947cffdc9fbb5
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-02-10 16:09:28 -0500

    Update main.js

--- ultralangscript/main.js
+++ ultralangscript/main.js
@@ -1,3 +1,20 @@
 var u = {
-  el: function(selector,node) {return document.querySelectorAll(selector)[node];}
+  el: function(selector,node) {return document.querySelectorAll(selector)[node];},
+  html: function(code) {return code;}
 };
+function s(code) {
+this.innerHTML=code;
+}
+function l(code) {
+this.href = code;
+}
+function st(code) {
+this.style = code;
+}
+
+/*
+Examples:
+u.el("#hi",0).s(u.html("<h1>hi</h1>"));
+u.el("a",0).l(example.com);
+u.el(".hi",0).st("color: red;");
+*/
