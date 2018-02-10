299ec4c8e11972d82c42400980e6c7ff838ddca5
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-02-10 10:30:21 -0500

    Update read.js

--- read.js
+++ read.js
@@ -4,10 +4,25 @@ a: ['say'],
   b: [function(params) {alert(params[1]);}]
 }
 };
+function splitsplit(t,c,n) {
+  var rtrnlst = [""];
+  var nn = 0;
+  for(var i=0; i<t.length; i++) {
+    if (t[i+nn]===n) {
+    nn++
+    }
+  if (t[i+nn]===c && nn%2===0) {
+      rtrnlst.push("");
+      } else {
+      rtrnlst[rtrnlst.length-1] += t[i+nn];
+      }
+  }
+  return rtrnlst;
+}
 var parameters = [];
 var js = ['say'];
 function readFunction(code) {
-parameters = code.split("/");
+parameters = splitsplit(code,"/","$");
   if (js.indexOf(parameters[0])!==-1) {
 codes.javascript.b[codes.javascript.a.indexOf(parameters[0])](parameters);
 }
