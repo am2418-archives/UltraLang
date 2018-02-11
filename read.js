ac15454d60dab8b77c7cbff2ca99ffc68726faca
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-02-11 14:40:17 -0500

    Update read.js

--- read.js
+++ read.js
@@ -5,16 +5,22 @@ a: ['say','ask'],
   b: [function(params) {alert(params[1]);},function(params) {result=prompt(params[1],params[2]);}]
 }
 };
-function splitsplit(t,c,n) {
+function splitsplit(t,c,n,nnnn) {
   var rtrnlst = [""];
   var nn = 0;
+  var nnnnn = 0;
   for(var i=0; i<t.length; i++) {
         if (t[i+nn]===n) {
-    nn++
+    nn++;
+         nnnnn++; 
+    }
+     if (t[i+nn]===nnnn) {
+    nn++;
+       nnnnn--;
     }
     if (i+nn<t.length) {
 
-  if (t[i+nn]===c && nn%2===0) {
+  if (t[i+nn]===c && nnnnn===0) {
       rtrnlst.push("");
       } else {
       rtrnlst[rtrnlst.length-1] += t[i+nn];
